cff251bc74111158367360794d8e1f1b8986072f
spruha <spruhanayak2004@gmail.com>
2024-01-19 11:30:12 -0800

    setting up

---
+++ lab2_part3.c
@@ -0,0 +1,21 @@
+// Name: Sarah Hallam, Aiden Jacob, and Spruha Nayak
+// Assignment: Lab 2 part 
+// Description: 
+
+#include "globals.h"
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+int main(void) {
+   init();  //initialize board hardware
+
+   while(1){
+   }
+   
+   return 0;
+}
