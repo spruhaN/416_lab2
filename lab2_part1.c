816cfdd7f41d90a12e389052041679107179c595
spruha <spruhanayak2004@gmail.com>
2024-01-29 11:05:46 -0800

    what

--- lab2_part1.c
+++ lab2_part1.c
@@ -11,11 +11,47 @@
 #include <avr/interrupt.h>
 
 
+int motor(u08 num, int8_t speed);
+
 int main(void) {
    init();  //initialize board hardware
+   int val0 = 0;
+   int val1 = 0;
 
-   while(1){
-   }
-   
+   //while(1){
+      val0 = motor(0,-100);
+      val1 = motor(1,-100);
+      // print_num(val0);
+      // clear_screen();
+   //}
    return 0;
 }
+
+// num(0 is left and 1 is right)
+// speed -100 - 100
+int motor(u08 num, int8_t speed){
+   //if ((num == 1 || num == 0) && (speed<= 100 && speed >= -100)){
+      // conversion
+      speed = floor(.3*speed + 127);
+      print_num(speed);
+      clear_screen();
+      // set_servo(num, speed);
+      return speed;
+   //}else{
+      //return -1;
+   //}
+   //return speed;
+}
+
+int gradually_inc(){
+   int val = 0;
+   while(1){
+      motor(0,val);
+      motor(1,val);
+      if (val < 100){
+         val++;
+      }
+   }
+}
+
+
--- lab2_part2.c
+++ lab2_part2.c
@@ -15,6 +15,25 @@ int main(void) {
    init();  //initialize board hardware
 
    while(1){
+      set_servo(0,127);
+      set_servo(1,127);
+      _delay_ms(100);
+      int pin0 = analog(0);
+      int pin1 = analog(1);
+      int pin2 = analog(2);
+      int pin3 = analog(3);
+      lcd_cursor(0,0);
+      print_num(pin0);
+      lcd_cursor(0,1);
+      print_num(pin1);
+      lcd_cursor(5,0);
+      print_num(pin2);
+      lcd_cursor(5,1);
+      print_num(pin3);
+      _delay_ms(1000);
+      clear_screen();
+      // int pin2 = analog(2);
+      // int pin3 = analog(3);
    }
    
    return 0;
